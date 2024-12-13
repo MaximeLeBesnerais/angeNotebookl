4601bc6f06b02d23a957cc5e4ae88e76b3c8e3a7
Maxime Le Besnerais <maxime.le-besnerais@epitech.eu>
2024-12-13 14:49:49 +0800

    Change file opening mode to append and initialize contact fields to NULL

--- main.c
+++ main.c
@@ -1,16 +1,23 @@
 #include "helpers.h"
 
 int main() {
-    FILE *file = fopen("contacts_book.csv", "w");
+    FILE *file = fopen("contacts_book.csv", "a+");
     if (file == NULL) {
         printf("Error opening file!\n");
         return 1;
     }
     contact contacts[15];
+    for (int i = 0; i < 15; i++) {
+        contacts[i].name = NULL;
+        contacts[i].phone = NULL;
+        contacts[i].email = NULL;
+    }
+
     read_book(contacts, file);
     if (!data_validation(contacts)) {
         printf("\033[1;31m Data validation failed\n");
         printf("\033[0m");
+        fclose(file);
         return 1;
     }
 
