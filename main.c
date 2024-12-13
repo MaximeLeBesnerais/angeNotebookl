06b08f921e31ffbc187f7cdfabb1856f71e4c324
Maxime Le Besnerais <maxime.le-besnerais@epitech.eu>
2024-12-13 14:35:43 +0800

    Implement file handling and data validation in main function

--- main.c
+++ main.c
@@ -1,18 +1,20 @@
-#include <string.h>
-
-typedef enum {
-    BUSINESS,
-    PERSONAL,
-    OFFICE
-} contact_type;
-
-typedef struct {
-    char *name;
-    char *phone;
-    contact_type type;
-    char *email;
-} contact;
+#include "helpers.h"
 
 int main() {
+    FILE *file = fopen("contacts_book.csv", "w");
+    if (file == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+    contact contacts[15];
+    read_book(contacts, file);
+    if (!data_validation(contacts)) {
+        // in red, data validation failed
+        printf("\033[1;31m Data validation failed\n");
+        // reset color
+        printf("\033[0m");
+        return 1;
+    }
+
     return 0;
 }
