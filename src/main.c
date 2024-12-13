ee1291b2c4d756e1b054c7a064983ef1eea19e4b
Maxime Le Besnerais <maxime.le-besnerais@epitech.eu>
2024-12-13 15:18:35 +0800

    Refactor main.c to include tools and contact handling, and implement a menu for contact management

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
-#include "helpers.h"
+#include "tools.h"
+#include "contact_handling.h"
 
 void data_setup(contact contacts[], FILE **file) {
     *file = fopen("contacts_book.csv", "a+");
@@ -32,7 +33,41 @@ int main() {
     contact contacts[15];
     data_setup(contacts, &file);
     bool running = true;
+    short option;
+
     while (running) {
+        display_menu();
+        puts("Enter an option: ");
+        char buffer[10];
+        if (fgets(buffer, 10, stdin) == NULL) {
+            puts("Error reading input\n");
+            fclose(file);
+            exit(1);
+        }
+        option = atoi(buffer);
+        switch (option) {
+            case 1:
+                puts("Add contact\n");
+                break;
+            case 2:
+                puts("Delete contact\n");
+                break;
+            case 3:
+                puts("Search contact\n");
+                break;
+            case 4:
+                puts("Display all contacts\n");
+                break;
+            case 5:
+                running = false;
+                break;
+            default:
+                puts("Invalid option!\n");
+                break;
+        }
+        for (int i = 0; i < 10; i++) {
+            buffer[i] = 0;
+        }
     }
     return 0;
 }
