2a7e0b9adf3a73930773b4eba49f372a1084a03b
Maxime Le Besnerais <maxime.le-besnerais@epitech.eu>
2024-12-13 20:41:13 +0800

    Implement file writing and exit process for contact management

--- src/main.c
+++ src/main.c
@@ -1,7 +1,8 @@
 #include "tools.h"
 #include "contact_handling.h"
 
-void data_setup(contact contacts[], FILE **file) {
+void data_setup(contact contacts[], FILE **file)
+{
     *file = fopen("contacts_book.csv", "a+");
     if (*file == NULL) {
         printf("Error opening file!\n");
@@ -22,6 +23,42 @@ void data_setup(contact contacts[], FILE **file) {
     }
 }
 
+bool write_to_file(contact contacts[], FILE *file) {
+    fclose(file);
+    file = fopen("contacts_book.csv", "w+");
+    if (file == NULL) {
+        red_text("Critical error opening file\n");
+        exit(1);
+    }
+    rewind(file);
+    for (int i = 0; i < 15; i++) {
+        if (contacts[i].name == NULL)
+            continue;
+        fprintf(file, "%s, %s, %d, %s\n", contacts[i].name, contacts[i].phone,
+                contacts[i].type, contacts[i].email);
+    }
+    fclose(file);
+    file = fopen("contacts_book.csv", "a+");
+    return true;
+}
+
+void exit_process(wrapper_contact contacts, FILE *file) {
+    puts("Exit key pressed\n");
+    if (contacts.modified) {
+        write_to_file(contacts.list, file);
+        puts("All contacts saved\n");
+    }
+    for (int i = 0; i < 15; i++)
+    {
+        if (contacts.list[i].name != NULL)
+        {
+            free(contacts.list[i].name);
+            free(contacts.list[i].phone);
+            free(contacts.list[i].email);
+        }
+    }
+}
+
 int main() {
     FILE *file;
     wrapper_contact contacts;
@@ -35,44 +72,40 @@ int main() {
         puts("Enter an option: ");
         char buffer[10];
         if (fgets(buffer, 10, stdin) == NULL) {
-            puts("Exit key pressed\n");
-            fclose(file);
-            exit(0);
+            exit_process(contacts, file);
+            running = false;
+            break;
         }
         clear_screen();
         option = atoi(buffer);
         switch (option) {
-            case 1:
-                if (!add_contact(&contacts)) {
-                    puts("There might be an error with the source file\n");
-                }
-                break;
-            case 2:
-                if (!delete_contact(&contacts)) {
-                    puts("There might be an error with the source file\n");
-                }
-                break;
-            case 3:
-                if (!edit_contact(&contacts)) {
-                    puts("There might be an error with the source file\n");
-                }
-                break;
-            case 4:
-                puts("Do you want to filter by type?");
-                puts("Write 1 for Business, 2 for Personal, 3 for Office,"\
-                " or any other key for all contacts");
-                if (fgets(type, 10, stdin) == NULL) {
-                    display_all_contacts(&contacts, UNKNOWN);
-                }
-                display_all_contacts(&contacts, getFilterType(type));
-                break;
-            case 5:
-                running = false;
-                break;
-            default:
-                puts("Invalid option!\n");
-                break;
+        case 1:
+            add_contact(&contacts);
+            break;
+        case 2:
+            delete_contact(&contacts);
+            break;
+        case 3:
+            edit_contact(&contacts);
+            break;
+        case 4:
+            puts("Do you want to filter by type?");
+            puts("Write 1 for Business, 2 for Personal, 3 for Office,"
+                 " or any other key for all contacts");
+            if (fgets(type, 10, stdin) == NULL)
+                display_all_contacts(&contacts, UNKNOWN);
+            display_all_contacts(&contacts, getFilterType(type));
+            break;
+        case 5:
+            running = false;
+            exit_process(contacts, file);
+            break;
+        default:
+            puts("Invalid option!\n");
+            break;
         }
+        buffer[0] = '\0';
     }
+    fclose(file);
     return 0;
 }
