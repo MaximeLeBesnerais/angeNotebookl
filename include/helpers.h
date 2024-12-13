57c732ec8c46be25c84714086147d739cfd2ac40
Maxime Le Besnerais <maxime.le-besnerais@epitech.eu>
2024-12-13 15:17:33 +0800

    Move away contact handling functions and types from helpers and tools

--- include/helpers.h
+++ include/helpers.h
@@ -6,30 +6,7 @@
 
 #ifndef HELPERS_H
 #define HELPERS_H
-#include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
-typedef enum
-{
-    BUSINESS,
-    PERSONAL,
-    OFFICE
-} contact_type;
-
-typedef struct
-{
-    char *name;
-    char *phone;
-    contact_type type;
-    char *email;
-    short entry;
-} contact;
-
-void contact_print(contact contacts);
-void read_book(contact *contacts, FILE *file);
-bool data_validation(contact *contacts);
 void red_text(char *text);
 
 #endif // HELPERS_H
