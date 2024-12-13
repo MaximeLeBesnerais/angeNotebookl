8040fc1ba09acfa5da57b956890bbda3f435b94e
Maxime Le Besnerais <maxime.le-besnerais@epitech.eu>
2024-12-13 14:35:57 +0800

    Refactor data validation error handling in main function

--- main.c
+++ main.c
@@ -9,9 +9,7 @@ int main() {
     contact contacts[15];
     read_book(contacts, file);
     if (!data_validation(contacts)) {
-        // in red, data validation failed
         printf("\033[1;31m Data validation failed\n");
-        // reset color
         printf("\033[0m");
         return 1;
     }
